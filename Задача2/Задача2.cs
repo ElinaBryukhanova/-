5bc8c0dc273425f93bbaccf554586def982894f8
Александр Брюханов <alexbruyhanov@yandex.ru>
2022-08-13 00:46:21 +0700

    ДомашнееЗаданиеС#

---
+++ Задача2/Задача2.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console. WriteLine("Введите число");
+int N1 = Convert.ToInt32(Console.ReadLine());
+Console. WriteLine("Введите число");
+int N2 = Convert.ToInt32(Console.ReadLine());
+if(N1 > N2) {
+    Console.WriteLine("N1 больче чем N2");
+}
+else if (N1 == N2) {
+    Console.WriteLine("N1 равен N2");
+}
+else {
+    Console.WriteLine("N2 больче чем N1");
+}
---
+++ Задача4/Задача4.cs
@@ -0,0 +1,9 @@
+﻿// Задача 4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console. WriteLine("Введите число");
+int N1 = Convert.ToInt32(Console.ReadLine());
+Console. WriteLine("Введите число");
+int N2 = Convert.ToInt32(Console.ReadLine());
+Console. WriteLine("Введите число");
+int N3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Math.Max(N1, Math.Max(N2, N3)));
